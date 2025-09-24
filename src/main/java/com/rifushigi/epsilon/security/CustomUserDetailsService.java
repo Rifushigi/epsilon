1cb18e2eac77a3448ff46f9a35b79549d9ca238d
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 19:22:59 +0100

    fix: minor changes

--- src/main/java/com/rifushigi/epsilon/security/CustomUserDetailsService.java
+++ src/main/java/com/rifushigi/epsilon/security/CustomUserDetailsService.java
@@ -1,11 +1,12 @@
 package com.rifushigi.epsilon.security;
 
 import com.rifushigi.epsilon.service.UserService;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserService userService;
