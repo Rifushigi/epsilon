d664be577afb47c4dd5d19969d441fdba4b5dde2
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 15:57:51 +0100

    feat: create dao for Click

---
+++ src/main/java/com/rifushigi/epsilon/dao/ClickRepository.java
@@ -0,0 +1,25 @@
+package com.rifushigi.epsilon.dao;
+
+import com.rifushigi.epsilon.entity.Click;
+import com.rifushigi.epsilon.entity.ShortUrl;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClickRepository extends JpaRepository<Click, UUID> {
+
+    Page<Click> findByShortUrlOrderByClickedAtDesc(ShortUrl shortUrl, Pageable pageable);
+
+    @Query("select c from Click c where c.shortUrl = :shortUrl")
+    Page<Click> findLatestClicksByShortUrl(@Param("shortUrl") ShortUrl shortUrl, Pageable pageable);
+
+    @Query("select c from Click c where c.shortUrl = :shortUrl group by c.country order by COUNT(c) desc")
+    Page<Click> getClickStatsByCountry(@Param("shortUrl") ShortUrl shortUrl, Pageable pageable);
+
+}
