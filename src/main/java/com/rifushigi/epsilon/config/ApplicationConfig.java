91e64aa94786c95c0c042524c9443db992aaeb9c
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 19:22:33 +0100

    feat: configure application with password encoder and auth provider

--- src/main/java/com/rifushigi/epsilon/config/ApplicationConfig.java
+++ src/main/java/com/rifushigi/epsilon/config/ApplicationConfig.java
@@ -1,10 +1,28 @@
 package com.rifushigi.epsilon.config;
 
+import com.rifushigi.epsilon.dao.UserRepository;
+import com.rifushigi.epsilon.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class ApplicationConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public ApplicationConfig(CustomUserDetailsService customUserDetailsService){
+        this.customUserDetailsService = customUserDetailsService;
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(customUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
