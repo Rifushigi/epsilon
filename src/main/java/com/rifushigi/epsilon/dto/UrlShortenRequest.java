c821effe064d36a837e3c99cbe11746e54da1d80
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 16:28:21 +0100

    feat: create dto for url shortening request

---
+++ src/main/java/com/rifushigi/epsilon/dto/UrlShortenRequest.java
@@ -0,0 +1,20 @@
+package com.rifushigi.epsilon.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record UrlShortenRequest(
+        @NotBlank(message = "URL is required")
+        String url,
+
+        @Positive(message = "TTL must be positive")
+        Long ttlSeconds,
+
+        @Size(min = 3, max = 20, message = "Custom short code must be between 3 and 20 characters")
+        String customShortCode
+) {
+    public UrlShortenRequest(String url, Long ttlSeconds){
+        this(url, ttlSeconds, null);
+    }
+}
