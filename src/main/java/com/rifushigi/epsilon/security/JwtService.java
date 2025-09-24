8dd986f2323881a879d1bdc755f67701e5959121
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 18:24:52 +0100

    feat: create jwt service

---
+++ src/main/java/com/rifushigi/epsilon/security/JwtService.java
@@ -0,0 +1,75 @@
+package com.rifushigi.epsilon.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtService.class);
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
+    @Value("${JWT_EXPIRATION_MS}")
+    private int jwtExpirationMs;
+
+    private SecretKey getSigningKey(){ return Keys.hmacShaKeyFor(jwtSecret.getBytes()); }
+
+    public String generateToken(Authentication authentication){
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        long now = System.currentTimeMillis();
+
+        return Jwts.builder()
+                .subject(userPrincipal.getUsername())
+                .claim("authorities", userPrincipal.getAuthorities())
+                .issuedAt(new Date())
+                .expiration( new Date(now + jwtExpirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        try{
+            final String username = extractUsername(token);
+            return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+        } catch (JwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+}
