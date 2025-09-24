c1ed10bf2dd6504252a902fdd8473ba4b3765131
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 23:35:06 +0100

    feat: minor changes

--- src/main/java/com/rifushigi/epsilon/security/CustomUserDetailsService.java
+++ src/main/java/com/rifushigi/epsilon/security/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -15,6 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userService = userService;
     }
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
         return userService.findByUsernameOrEmail(usernameOrEmail)
