c94e8f4db400858c31beed1a573a0df2ad17323c
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 21:11:25 +0100

    feat: create user service

---
+++ src/main/java/com/rifushigi/epsilon/service/UserService.java
@@ -0,0 +1,74 @@
+package com.rifushigi.epsilon.service;
+
+import com.rifushigi.epsilon.dao.UserRepository;
+import com.rifushigi.epsilon.dto.AuthResponse;
+import com.rifushigi.epsilon.dto.LoginRequest;
+import com.rifushigi.epsilon.dto.RegistrationRequest;
+import com.rifushigi.epsilon.entity.User;
+import com.rifushigi.epsilon.security.JwtService;
+import com.rifushigi.epsilon.security.UserPrincipal;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JwtService jwtService){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+    public User registerUser(RegistrationRequest request){
+        if (userRepository.existsByUsername(request.username())){
+            throw new RuntimeException("Username is already taken");
+        }
+
+        if (userRepository.existsByEmail(request.email())){
+            throw new RuntimeException("Email is already is already in use");
+        }
+
+        User user = new User();
+        user.setEmail(request.email());
+        user.setUsername(request.username());
+        user.setPassword(passwordEncoder.encode(request.password()));
+
+        return userRepository.save(user);
+    }
+
+    public AuthResponse loginUser(LoginRequest request){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.usernameOrEmail(), request.password())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtService.generateToken(authentication);
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        return new AuthResponse( token, userPrincipal.getUsername(), userPrincipal.getEmail());
+    }
+
+    public User getCurrentUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public Optional<User> findByUsernameOrEmail(String usernameOrEmail){
+        return userRepository.findByUsernameOrEmail(usernameOrEmail);
+    }
+}
