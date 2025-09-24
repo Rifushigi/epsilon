6bdb786d653a94a5ac516aec0b36dd0f907f071d
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 13:54:25 +0100

    feat: add email data field

--- src/main/java/com/rifushigi/epsilon/entity/User.java
+++ src/main/java/com/rifushigi/epsilon/entity/User.java
@@ -1,7 +1,9 @@
 package com.rifushigi.epsilon.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -31,6 +33,11 @@ public class User {
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     @Column(unique = true, nullable = false)
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 6 characters")
