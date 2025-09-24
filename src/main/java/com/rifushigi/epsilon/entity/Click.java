5c59a567823c7afcaa580949b55566dc2adff678
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 14:35:01 +0100

    feat: create entity for analytics

---
+++ src/main/java/com/rifushigi/epsilon/entity/Click.java
@@ -0,0 +1,43 @@
+package com.rifushigi.epsilon.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "clicks", indexes = {
+        @Index(name = "idx_short_url_id", columnList = "short_url_id"),
+        @Index(name = "idx_clicked_at", columnList = "clicked_at")
+})
+public class Click {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false, columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    // bidirectional relationship with short url?
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "short_url_id", nullable = false)
+    private ShortUrl shortUrl;
+
+    @Column(name = "ip_address", length = 45)
+    private String ipAddress;
+
+    @Column(name = "user_agent", columnDefinition = "TEXT")
+    private String userAgent;
+
+    @Column(name = "referer", length = 2048)
+    private String referer;
+
+    @CreationTimestamp
+    @Column(name = "clicked_at", nullable = false, updatable = false)
+    private LocalDateTime clickedAt;
+
+}
