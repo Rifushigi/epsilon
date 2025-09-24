039ee01f2d94346bab29d6c7816f828b2f64f5e6
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 11:58:00 +0100

    Project template

---
+++ src/test/java/com/rifushigi/epsilon/EpsilonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifushigi.epsilon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsilonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
