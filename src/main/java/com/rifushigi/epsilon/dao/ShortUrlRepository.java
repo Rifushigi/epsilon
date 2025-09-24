c72ff2f2e9347ba16adec86bb7a18ee2706e84cf
Rifushigi <rifushigi.teda@gmail.com>
2025-09-24 15:43:23 +0100

    fix: add JpaRepo interface

--- src/main/java/com/rifushigi/epsilon/dao/ShortUrlRepository.java
+++ src/main/java/com/rifushigi/epsilon/dao/ShortUrlRepository.java
@@ -4,6 +4,7 @@ import com.rifushigi.epsilon.entity.ShortUrl;
 import com.rifushigi.epsilon.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +15,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ShortUrlRepository {
+public interface ShortUrlRepository extends JpaRepository<ShortUrl, UUID> {
 
     Optional<ShortUrl> findByShortCode(String shortCode);
 
